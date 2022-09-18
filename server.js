e978befb7a5d9d3d3607675ded121ad3195df473
mdraihan34988 <swammia908@gmail.com>
2022-09-18 17:26:36 +0600

    add

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+router.render = (req, res) => {
+    const path = req.path;
+    const method = req.method;
+
+    if (
+        path.includes("/users") &&
+        (method === "GET")
+    ) {
+        res.locals.data = res.locals.data.map((user) => {return {
+            id : user.id,
+            email : user.email,
+            name : user.name
+        }})
+    }
+
+    res.json(res.locals.data);
+};
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
